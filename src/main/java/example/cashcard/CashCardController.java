a124fdd13d0786ec71190b529df706eab311a7ea
Alberto Santos <eks.alberto@gmail.com>
2024-08-29 00:17:45 +1000

    Add POST cashcard endpoint

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,11 +1,10 @@
 package example.cashcard;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Optional;
 
 @RestController
@@ -28,4 +27,14 @@ public class CashCardController {
             return ResponseEntity.notFound().build();
         }
     }
+
+    @PostMapping
+    private ResponseEntity<String> createCashCard(@RequestBody CashCard newCashCardRequest,
+                                                  UriComponentsBuilder ucb) {
+        CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
+        URI locationOfNewCashCard = ucb.path("/cashcards/{id}")
+                .buildAndExpand(savedCashCard.id()).toUri();
+        return ResponseEntity.created(locationOfNewCashCard).build();
+    }
+
 }
