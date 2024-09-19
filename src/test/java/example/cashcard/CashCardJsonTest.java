9a4c127ea28fe273f98f67594e6502cd45235f96
Alberto Santos <eks.alberto@gmail.com>
2024-09-19 23:02:55 +1000

    Add GET many cashcard endpoint

--- src/test/java/example/cashcard/CashCardJsonTest.java
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -1,5 +1,7 @@
 package example.cashcard;
 
+import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
@@ -14,6 +16,20 @@ public class CashCardJsonTest {
 
     @Autowired
     private JacksonTester<CashCard> json;
+
+    @Autowired
+    private JacksonTester<CashCard[]> jsonList;
+
+    private CashCard[] cashCards;
+
+    @BeforeEach
+    void setUp() {
+        cashCards = Arrays.array(
+                new CashCard(99L, 123.45),
+                new CashCard(100L, 1.00),
+                new CashCard(101L, 150.00)
+        );
+    }
 
     @Test
     void cashCardSerializationTest() throws IOException {
@@ -41,5 +57,25 @@ public class CashCardJsonTest {
         assertThat(json.parse(expectedJson)).isEqualTo(new CashCard(99L, 123.45));
         assertThat(json.parseObject(expectedJson).id()).isEqualTo(99);
         assertThat(json.parseObject(expectedJson).amount()).isEqualTo(123.45);
+    }
+
+    @Test
+    void cashCardListSerializationTest() throws IOException {
+        assertThat(jsonList.write(cashCards)).isStrictlyEqualToJson("list.json");
+    }
+
+    @Test
+    void cashCardListDeserializationTest() throws IOException {
+        String expectedJson = """
+                [
+                  {"id": 99, "amount": 123.45 },
+                  {"id": 100, "amount": 1.00 },
+                  {"id": 101, "amount": 150.00 }
+                ]
+                """;
+
+        assertThat(jsonList.parse(expectedJson)).isEqualTo(cashCards);
+        assertThat(jsonList.parseObject(expectedJson)[0].id()).isEqualTo(99);
+        assertThat(jsonList.parseObject(expectedJson)[1].amount()).isEqualTo(1.00);
     }
 }
